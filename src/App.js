71533370cd450d2728776e90a19b994f5d0b27bf
divyansh <divyansh@hubilo.com>
2021-12-26 23:54:34 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -2,34 +2,47 @@
 //using redux
 
 import './App.css';
-import { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import {
+    decrement,
+    increment,
+    reset,
+    logIn,
+    logOut,
+} from "./Redux/actions/index";
 
-const App = () =>{
-  const [curNum,updNum] = useState(0); // hook declaration needs to be inside function
-  const incNum = () =>{
-    updNum(curNum+1); //issue with ++ and -- ?
-  };
-  const decNum = () =>{
-    if(curNum>0){
-      updNum(curNum-1);
-    }
-    else
-      alert('The least value of count can be 0.')
-  };
-  return(
-    // React Fragment
-    <> 
-      <div className='main'>
-        <div className='centerDiv'>
-          <h1>{curNum}</h1>
-          <div className='btn'>
-            <button onClick={incNum}>Increase</button>
-            <button onClick={decNum}>Decrease</button>
-          </div>
+function App() {
+    const counter = useSelector((state)=>state.counterReducer);
+    const auth = useSelector((state)=>state.authReducer);
+    const dispatch = useDispatch();
+    
+    return(
+        <div className="App">
+            <h1>
+                A little Redux Project.
+            </h1>
+            <h3>Counter</h3>
+            <h3>{counter}</h3>
+            <button onClick={() => dispatch(increment())}>Increase</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+            <button onClick={() => dispatch(decrement())}>Decrease</button>
+
+            <h2>For Logged in users only</h2>
+            <button onClick={() => dispatch(logIn())}>Login</button>
+            <button onClick={() => dispatch(logOut())}>Logout</button>
+            {auth ? (
+                <div>
+                <p>
+                    This is implemented using Redux.
+                </p>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
-      </div>
-    </>
-  )
+    );
 }
+
 export default App;
 // to use state variable with functions we use hooks, in class we can use state and props directly
